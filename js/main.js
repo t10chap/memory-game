4e2e0d6beffad9349c8bf53cdea686152a2ff0b4
Tevin Rawls <tevinrawls@gmail.com>
2018-06-17 00:52:50 -0700

    trying to fix submodule

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+		}
+		else{
+		alert("Sorry, try again.");
+		}
+	}
+}
+
+var flipCard = function(cardID){
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
